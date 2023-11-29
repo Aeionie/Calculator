6ba49e80b623e681d0371f0e37475a307c991f96
Aeionie <asule@gmail.com>
2023-11-29 05:53:58 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+// class Calculator {
+//   constructor(display, result) {
+//     this.display = display;
+//     this.result = result;
+//     this.clear()
+//   }
+//   clear() {
+//     this.displayOperand = "";
+//     this.resultOperand = "";
+//     this.operation = undefined
+//   }
+
+//   delete() {
+//   }
+//   appendNumber(number) {
+
+//   }
+//   chooseOperantion(operation) {
+
+//   }
+//   compute() {
+
+//   }
+//   updateDisplay() {
+
+//   }
+// }
+
+// const numberButtons = document.querySelector("[data-number]");
+// const display = document.getElementById(["input_display"]);
+// const result = document.getElementById(["calc display"]);
+// const operationButtons = document.querySelector("[data-operatio]");
+// const deleteButton = document.querySelector("[data-delete]");
+// const equalsButton = document.querySelector("[data-equals]");
+
+// const calculator = new Calculator(display, result)
+
+// normalButton.forEach(button => {
+//   button.addEventListener('click', () =>){
+//     calculator.appendNumber(button.innerText)
+//     calculator.updateDisplay()
+//   }
+// })
+
